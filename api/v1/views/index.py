d9fac1deee911f7145aaca658e820026d45a5ecf
oryankibandi <shreckoryan@gmail.com>
2022-10-24 22:46:43 +0300

    status of your api

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+from api.v1.views import app_views
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+
+@app_views.route('/status')
+def status():
+    """returns response status"""
+    return (jsonify({'status': 'OK'}))
