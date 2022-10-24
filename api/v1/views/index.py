b7d8a02b741e8ae4f86cd733deba4f360417769a
oryankibandi <shreckoryan@gmail.com>
2022-10-25 00:36:41 +0300

    added /stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,17 @@ Registers blueprint and adds the status route
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
@@ -15,3 +26,12 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def status():
     """returns response status"""
     return (jsonify({'status': 'OK'}))
+
+@app_views.route('/api/v1/stats')
+def stats():
+    """Retrieves the number of each objects by type"""
+    obj_dict = {}
+    for clss in classes:
+        obj_dict[clss] = storage.count()
+
+    return obj_dict
